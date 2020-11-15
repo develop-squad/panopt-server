f84a328a368a6140bbc905ee862a17a60f5c6486
LimeCake <limecake23@gmail.com>
2020-11-15 16:23:41 +0900

    base rest http server

--- program/README.md
+++ program/README.md
@@ -11,7 +11,7 @@ IP: 49.247.197.181
 PORT: 80  
 
 ## RESPONSE FORMAT (JSON)  
-`{ code: RESPONSE_CODE }`  
+`{ "code": RESPONSE_CODE }`  
 
 데이터 응답 포함 시 해당 필드 포함  
 메시지 응답 포함 시 `messages` 필드 포함  
@@ -23,7 +23,7 @@ PORT: 80
 1: request success  
 
 ## RESPONSE_MESSAGE (JSON)  
-`{ type: MESSAGE_TYPE , title: ”제목”, message: ”메시지”, datetime: ”시각” }`  
+`{ "type": MESSAGE_TYPE , "title": ”제목”, "message": ”메시지”, "datetime": ”시각” }`  
 
 프로세스 정보 포함 시 `process` 필드 포함  
 패킷 정보 포함 시 `packet` 필드 포함  
@@ -34,6 +34,23 @@ PORT: 80
 2: 알림 메시지  
 3: 경고 메시지  
 4: 위험 메시지  
+
+<h1 align="center">에코 테스트<h4 align="center">데이터 송수신 테스트를 위한 에코입니다</h4></h1>
+
+## PATH
+`/echo`  
+
+## REQUEST METHOD  
+POST,GET  
+
+## ARGUMENT  
+(string) `data`: test data  
+
+## OUTPUT  
+RESPONSE FORMAT  
+
+### OUTPUT EXAMPLE  
+`{ "type": GET/POST, "data": received data }`  
 
 <h1 align="center">서버 연결<h4 align="center">서버에 연결하고자 할때 초기 1회 호출합니다</h4></h1>
 
@@ -50,7 +67,7 @@ POST(,GET)
 RESPONSE FORMAT  
 
 ### OUTPUT EXAMPLE  
-`{ code: 1 }`  
+`{ "code": 1 }`  
 
 <h1 align="center">새로운 패킷 이벤트<h4 align="center">클라이언트에 새로운 패킷 이벤트 발생 시 호출합니다</h4></h1>
 
@@ -67,7 +84,7 @@ POST
 RESPONSE FORMAT  
 
 ### OUTPUT EXAMPLE  
-`{ code: 1 }`  
+`{ "code": 1 }`  
 
 <h1 align="center">새로운 프로세스 통신 이벤트<h4 align="center">클라이언트에 새로운 프로세스 통신 이벤트 발생시 호출합니다 </h4></h1>
 
@@ -84,9 +101,9 @@ POST
 RESPONSE FORMAT  
 
 ### OUTPUT SAMPLE  
-`{ code: 1 }`  
+`{ "code": 1 }`  
 
-<h1 align="center">알림 이벤트 풀링<h4 align="center">클라이언트로의 이벤트 알림이 있는지 확인합니다</h4></h1>
+<h1 align="center">알림 이벤트 폴링<h4 align="center">클라이언트로의 이벤트 알림이 있는지 확인합니다</h4></h1>
 
 ## PATH  
 `/users/{device}/event`  
@@ -103,4 +120,4 @@ RESPONSE FORMAT
 메시지 존재 시, `messages` 필드 포함  
 
 ### OUTPUT SAMPLE  
-`{ code: 1, messages: [{{ type: 1 , title: ”일반 메시지 제목”, message: ”일반 메시지 내용”, datetime: ”시각” }}, {{ type: 4 , title: ”위험 메시지 제목”, message: ”위험 메시지 내용”, datetime: ”시각”, process: “Bandizip.exe” }}] }`
+`{ "code": 1, "messages": [{{ "type": 1 , "title": ”일반 메시지 제목”, "message": ”일반 메시지 내용”, "datetime": ”시각” }}, {{ "type": 4 , "title": ”위험 메시지 제목”, "message": ”위험 메시지 내용”, "datetime": ”시각”, "process": “Bandizip.exe” }}] }`
--- program/main.py
+++ program/main.py
@@ -1,12 +1,95 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
+
+import json
+import constants
+import response
+import message
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/")
 def route_main():
-    return "route_main"
+    return constants.SERVER_NAME
+
+
+@app.route("/echo", methods=['GET', 'POST'])
+def route_echo():
+    data = ''
+    if request.method == 'GET':
+        data = request.args.get('data')
+    if request.method == 'POST':
+        data = request.form['data']
+    return {"type": request.method, "data": data}
+
+
+@app.route("/connect", methods=['GET', 'POST'])
+def route_connect():
+    device = ''
+    if request.method == 'GET':
+        device = request.args.get('device')
+    if request.method == 'POST':
+        device = request.form['device']
+    return response_normal(response.CODE_SUCCESS, None)
+
+
+@app.route("/users/<device>/packet", methods=['GET', 'POST'])
+def route_users_packet(device):
+    packet = ''
+    if request.method == 'GET':
+        response_normal(response.CODE_FAIL, 'REQUEST TYPE MUST BE POST')
+    if request.method == 'POST':
+        packet = request.form['data']
+    return response_normal(response.CODE_SUCCESS, None)
+
+
+@app.route("/users/<device>/process", methods=['GET', 'POST'])
+def route_users_process(device):
+    process = ''
+    if request.method == 'GET':
+        response_normal(response.CODE_FAIL, 'REQUEST TYPE MUST BE POST')
+    if request.method == 'POST':
+        process = request.form['data']
+    return response_normal(response.CODE_SUCCESS, None)
+
+
+@app.route("/users/<device>/event", methods=['GET', 'POST'])
+def route_users_event(device):
+    return response_normal(response.CODE_SUCCESS, None)
+
+
+@app.route("/manage/<password>/monitor", methods=['GET', 'POST'])
+def route_manage_monitor(password):
+    if password != constants.MANAGE_PASSWORD:
+        return response_normal(response.CODE_FAIL, "PASSWORD NOT MATCH")
+    return response_normal(response.CODE_SUCCESS, None)
+
+
+@app.route("/manage/<password>/<device>", methods=['GET', 'POST'])
+def route_manage_device(password, device):
+    if password != constants.MANAGE_PASSWORD:
+        return response_normal(response.CODE_FAIL, "PASSWORD NOT MATCH")
+    return response_normal(response.CODE_SUCCESS, None)
+
+
+@app.route("/manage/<password>/reset", methods=['GET', 'POST'])
+def route_manage_reset(password):
+    if password != constants.MANAGE_PASSWORD:
+        return response_normal(response.CODE_FAIL, "PASSWORD NOT MATCH")
+    return response_normal(response.CODE_SUCCESS, None)
+
+
+def response_normal(response_code, response_error):
+    if response_error is None:
+        return {"code": response_code}
+    else:
+        return {"code": response_code, "error": response_error}
+
+
+def response_message(response_code, messages):
+    return {"code": response_code, "messages": messages}
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port="5000")
+    app.run(host=constants.SERVER_IP, port=constants.SERVER_PORT)
